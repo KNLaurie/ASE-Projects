26568b0a018763320937335317b3f360a95b605f
KNLaurie <41209825+KNLaurie@users.noreply.github.com>
2018-08-01 13:44:42 -0700

    Update Mountain-Car.py

--- OpenAI/Mountain-Car.py
+++ OpenAI/Mountain-Car.py
@@ -3,7 +3,7 @@ from gym import wrappers
 env = gym.make('MountainCar-v0')
 
 
-Iterations = 10000
+Iterations = 6000
 
 best_policy = None 
 
@@ -11,11 +11,14 @@ best_policy = None
 
 def run(iteration, best_reward, best_policy, environment):
     max_steps = 200
+    reward = 0
     steps = 0
-    policy = np.random.rand(2)  
+    policy = (best_policy + np.random.rand(2)) - 0.5
     observation = env.reset()
     running = True
     obs_list = []
+    vel_list = []
+    print("Test Policy: " + str(policy))
     while running:
         #env.render()
         steps += 1
@@ -24,20 +27,27 @@ def run(iteration, best_reward, best_policy, environment):
                   
         else:
             action = 2
-        observation, reward, done, info = env.step(action)
-         
+        observation, r, done, info = env.step(action)
+        obs_list.append(observation[0])
+        vel_list.append(observation[1])
+        
         if done == True:
-            obs_list.append(observation[0])
-            reward += max(obs_list)
-               
-            if steps < best_reward or iteration == 0:
-                best_policy = policy
-                best_reward = steps
-               
+            if observation[0] >= 0.5:
                 running = False
-        if steps == max_steps:
-            
-            running = False
+        
+            elif steps == max_steps:
+                running = False
+    
+    
+    reward += max(obs_list)
+    reward -= steps
+    
+    if iteration == 0:
+        best_reward = reward
+    
+    elif reward  > best_reward:
+        best_policy = policy
+        best_reward = reward
     
      
             
@@ -45,15 +55,14 @@ def run(iteration, best_reward, best_policy, environment):
         
 
 def train(iter, environment):
-    best_policy = None 
+    best_policy = np.random.rand(2)
     best_reward = -1
     
     for i in range(iter):
         print("Episode: ", i + 1)
         best_policy, best_reward = run(i, best_reward, best_policy, environment)
-        print(best_policy)
-        
-        if i < 10 or i % 500 == 0:
+        print("Best policy: " + str(best_policy))
+        if i == 0 or i % 200 == 0:
             env = gym.wrappers.Monitor(environment, "/u/lauriek/PythonExercises/OpenAI/MCData", force = True)
             running = True
             observation = env.reset()
@@ -71,14 +80,15 @@ def train(iter, environment):
                 
                 observation, reward, done, info = env.step(action)
                 
-                if observation[0] >= 0.5:
-                    print("Episode " + str(i + 1) + " number of moves: " +  str(steps))
-                    running = False
-            
-               
-                elif max_steps == steps:
-                    print("Failure")
-                    running = False
+                if done == True:
+                    if observation[0] >= 0.5:
+                        print("Episode " + str(i + 1) + " number of moves: " +  str(steps))
+                        running = False
+                
+                    
+                    elif max_steps == steps:
+                        print("Failure")
+                        running = False 
         
     return best_policy
 
@@ -86,29 +96,26 @@ best_policy = train(Iterations, env)
 
 def test(policy):
     env = gym.make('MountainCar-v0')
-    
     for i in range(10):
         running = True
         observation = env.reset()
         max_steps = 500
         steps = 0
-        
         while running:
             env.render()
             steps += 1
-            
             if policy.dot(observation) <= 0:
-                action = 0     
+                action = 0
+                
             else:
                 action = 2
             
             observation, reward, done, info = env.step(action)
-            
             if observation[0] >= 0.5:
                 print("Episode " + str(i + 1) + " number of moves: " +  str(steps))
-                running = False           
+                running = False
+            
+               
             elif max_steps == steps:
                 running = False
-
 test(best_policy)
-    
